556eac28321db954d731cddbe631cc0d839968fe
haruxx <harukatarotaro@gmail.com>
2022-10-23 13:08:34 +0900

    Initial Commit

---
+++ watchPractice Watch App/watchPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchPracticeApp.swift
+//  watchPractice Watch App
+//
+//  Created by isao on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct watchPractice_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchPractice/watchPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchPracticeApp.swift
+//  watchPractice
+//
+//  Created by isao on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct watchPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
